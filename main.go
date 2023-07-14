61781628e48ff8ed39f6d2c870b293915c9309f8
Andres Blas Pujadas <andres.pujadas@liveoak.bank>
2023-07-13 21:31:15 -0300

    another version of the crawler, adding concurrent crawling with n amount of workers

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
+	"time"
 
 	"golang.org/x/net/html"
 )
+
+const crawlerWorkerAmount = 50
 
 func main() {
 	urlToCrawl, err := url.Parse("https://parserdigital.com/")
@@ -17,24 +21,69 @@ func main() {
 		log.Fatalf("url to crawl is invalid")
 	}
 
-	err = CrawlWebpage(urlToCrawl)
-	if err != nil {
-		log.Fatalf("error while crawling: %+v", err.Error())
+	pendingURLch := make(chan string) // Channel to hold pending URLs
+	visitedLinksMap := sync.Map{}
+
+	// Start the workers
+	for i := 0; i < crawlerWorkerAmount; i++ {
+		go crawlerWorker(fmt.Sprintf("Worker #%d", i), pendingURLch, &visitedLinksMap)
+	}
+
+	// Add the starting URL to the pending URLs channel
+	addLinksToPendingURLChannel("main", []string{urlToCrawl.String()}, pendingURLch, &visitedLinksMap)
+
+	select {
+	case <-time.After(time.Second * 20):
+	}
+
+	visitedLinksMap.Range(func(key, value any) bool {
+		fmt.Printf("key: %s, value: %s\n", key, value)
+		return true
+	})
+
+}
+
+func crawlerWorker(workerName string, pendingURLch chan string, visitedLinksMap *sync.Map) {
+	for linkToCrawl := range pendingURLch {
+		fmt.Printf("[%s]\tStarting to crawl link: %s\n", workerName, linkToCrawl)
+		visitedLinksMap.Store(linkToCrawl, true)
+
+		parsedLink, err := url.Parse(linkToCrawl)
+		if err != nil {
+			fmt.Printf("[%s]\tcould not parse link [%s]. ignoring.\n", workerName, linkToCrawl)
+			continue
+		}
+		links, err := CrawlWebpage(*parsedLink)
+		if err != nil {
+			fmt.Printf("[%s]\terror while crawling %s.", workerName, linkToCrawl)
+			continue
+		}
+
+		addLinksToPendingURLChannel(workerName, links, pendingURLch, visitedLinksMap)
 	}
 }
 
-func CrawlWebpage(webpageURL *url.URL) error {
+func addLinksToPendingURLChannel(workerName string, links []string, pendingURLch chan string, visitedLinksMap *sync.Map) {
+	for _, link := range links {
+		if _, ok := visitedLinksMap.Load(link); ok == false {
+			fmt.Printf("[%s]\tADDING LINK TO CHANNEL: %s\n", workerName, link)
+			pendingURLch <- link
+		}
+	}
+}
+
+func CrawlWebpage(webpageURL url.URL) ([]string, error) {
 	webpageContent := GetWebpageContent(webpageURL)
 
-	_, err := ExtractLinks(webpageURL, webpageContent)
+	links, err := ExtractLinks(webpageURL, webpageContent)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return links, nil
 }
 
-func GetWebpageContent(url *url.URL) string {
+func GetWebpageContent(url url.URL) string {
 	res, err := http.Get(url.String())
 	if err != nil {
 		log.Fatal(err)
@@ -48,7 +97,7 @@ func GetWebpageContent(url *url.URL) string {
 	return string(content)
 }
 
-func ExtractLinks(webpageURL *url.URL, webpageContent string) ([]string, error) {
+func ExtractLinks(webpageURL url.URL, webpageContent string) ([]string, error) {
 	parsedHtmlContent, err := html.Parse(strings.NewReader(webpageContent))
 	if err != nil {
 		return nil, err
@@ -57,11 +106,7 @@ func ExtractLinks(webpageURL *url.URL, webpageContent string) ([]string, error) 
 	links := searchDomainMatchingLinks(webpageURL, parsedHtmlContent)
 	linksWithoutDuplicates := removeDuplicates(links)
 
-	for _, linksWithoutDuplicate := range linksWithoutDuplicates {
-		fmt.Println(linksWithoutDuplicate)
-	}
-
-	return nil, nil
+	return linksWithoutDuplicates, nil
 }
 
 func removeDuplicates(links []string) []string {
@@ -78,7 +123,7 @@ func removeDuplicates(links []string) []string {
 	return uniqueSlice
 }
 
-func searchDomainMatchingLinks(webpageURL *url.URL, node *html.Node) []string {
+func searchDomainMatchingLinks(webpageURL url.URL, node *html.Node) []string {
 	var links []string
 	if node.Type == html.ElementNode && node.Data == "a" {
 		for _, attr := range node.Attr {
@@ -95,7 +140,7 @@ func searchDomainMatchingLinks(webpageURL *url.URL, node *html.Node) []string {
 	return links
 }
 
-func domainMatches(webpageURL *url.URL, hrefValue string) bool {
+func domainMatches(webpageURL url.URL, hrefValue string) bool {
 	hrefUrl, err := url.Parse(hrefValue)
 	if err != nil {
 		return false
