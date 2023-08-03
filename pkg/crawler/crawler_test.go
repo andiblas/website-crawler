9d7226b3d498f70ee8a0183e1793f38c330e6b46
Andres Blas Pujadas <andres.pujadas@liveoak.bank>
2023-08-03 20:30:24 -0300

    implement tests for bf crawler

--- pkg/crawler/crawler_test.go
+++ pkg/crawler/crawler_test.go
@@ -6,39 +6,57 @@ import (
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 
 	"golang.org/x/net/context"
 
 	"github.com/andiblas/website-crawler/pkg/fetcher"
 )
 
-const (
-	htmlWithSingleLink      = `<a href="https://test.com"/>`
-	htmlWithThreeLinks      = `<a href="https://test.com"/><a href="https://test.com/contact"/><a href="https://test.com/about-us"/>`
-	htmlWithLinksDepthThree = `<a href="https://test.com"/><a href="https://test.com/depth1"/><a href="https://test.com/depth1/depth2"/><a href="https://test.com/depth1/depth2/depth3"/>`
-)
+type errorCallbackArgs struct {
+	link url.URL
+	err  error
+}
 
 type mockFetcher struct {
-	webpageReader io.ReadCloser
-	throwError    error
+	webpageWithLinks map[string]string
+	throwError       error
 }
 
-func (m mockFetcher) FetchWebpageContent(_ url.URL) (io.ReadCloser, error) {
-	return m.webpageReader, m.throwError
+func newMockFetcher(throwError error) *mockFetcher {
+	webpageWithLinks := map[string]string{
+		"https://test.com":          `<a href="https://test.com"/><a href="https://test.com/contact"/><a href="https://test.com/about-us"/>`,
+		"https://test.com/contact":  `<a href="https://test.com"/><a href="https://test.com/depth3"/>`,
+		"https://test.com/about-us": `<a href="https://test.com"/><a href="https://test.com/contact"/><a href="https://test.com/about-us"/>`,
+		"https://test.com/depth3":   `<a href="https://test.com"/><a href="https://test.com/depth4"/>`,
+	}
+	return &mockFetcher{webpageWithLinks: webpageWithLinks, throwError: throwError}
 }
 
-func TestConcurrent_Crawl(t *testing.T) {
+func (m mockFetcher) FetchWebpageContent(urlToCrawl url.URL) (io.ReadCloser, error) {
+	if webpageHtml, ok := m.webpageWithLinks[urlToCrawl.String()]; ok {
+		return io.NopCloser(strings.NewReader(webpageHtml)), m.throwError
+	}
+	return io.NopCloser(strings.NewReader("")), m.throwError
+}
+
+func TestBreadthFirstCrawler_Crawl(t *testing.T) {
+	startUrl := "https://test.com"
 	canceledCtx, cancelFunc := context.WithCancel(context.Background())
 	cancelFunc()
-	testUrl, _ := url.Parse("https://test.com")
+	testUrl, _ := url.Parse(startUrl)
+	linkFoundCh := make(chan url.URL)
+	errorCallbackCh := make(chan errorCallbackArgs)
 	type fields struct {
 		fetcher fetcher.Fetcher
 	}
 	type args struct {
 		ctx            context.Context
 		urlToCrawl     url.URL
-		recursionLimit int
-		onNewLinkFound func(link url.URL)
+		depth          int
+		maxConcurrency int
+		linkFound      linkFoundCallback
+		errorCallback  crawlingErrorCallback
 	}
 	tests := []struct {
 		name    string
@@ -48,16 +66,15 @@ func TestConcurrent_Crawl(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "crawls a page with a single link and returns it",
-			fields: fields{fetcher: mockFetcher{
-				webpageReader: io.NopCloser(strings.NewReader(htmlWithSingleLink)),
-				throwError:    nil,
-			}},
+			name:   "crawls with only one depth step and returns only one link (the provided url)",
+			fields: fields{newMockFetcher(nil)},
 			args: args{
 				ctx:            context.Background(),
 				urlToCrawl:     *testUrl,
-				recursionLimit: 1,
-				onNewLinkFound: nil,
+				depth:          1,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound:      nil,
 			},
 			want: map[string]bool{
 				"https://test.com": true,
@@ -65,59 +82,168 @@ func TestConcurrent_Crawl(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "crawls a page with three links and returns them",
-			fields: fields{fetcher: mockFetcher{
-				webpageReader: io.NopCloser(strings.NewReader(htmlWithThreeLinks)),
-				throwError:    nil,
-			}},
+			name:   "crawls up two depth levels and returns all pages at that depth level without repeating links and ignoring deeper links",
+			fields: fields{newMockFetcher(nil)},
 			args: args{
 				ctx:            context.Background(),
 				urlToCrawl:     *testUrl,
-				recursionLimit: 2,
-				onNewLinkFound: nil,
+				depth:          2,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound:      nil,
 			},
 			want: map[string]bool{
 				"https://test.com":          true,
-				"https://test.com/about-us": true,
 				"https://test.com/contact":  true,
+				"https://test.com/about-us": true,
 			},
 			wantErr: false,
 		},
 		{
-			name: "crawls a page returns an error",
-			fields: fields{fetcher: mockFetcher{
-				webpageReader: nil,
-				throwError:    errors.New("error"),
-			}},
+			name:   "crawling way too deep should get all links",
+			fields: fields{newMockFetcher(nil)},
 			args: args{
 				ctx:            context.Background(),
 				urlToCrawl:     *testUrl,
-				recursionLimit: 1,
-				onNewLinkFound: nil,
+				depth:          100,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound:      nil,
+			},
+			want: map[string]bool{
+				"https://test.com":          true,
+				"https://test.com/contact":  true,
+				"https://test.com/about-us": true,
+				"https://test.com/depth3":   true,
+				"https://test.com/depth4":   true,
+			},
+			wantErr: false,
+		},
+		{
+			name:   "invalid depth",
+			fields: fields{newMockFetcher(nil)},
+			args: args{
+				ctx:            context.Background(),
+				urlToCrawl:     *testUrl,
+				depth:          -1,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound:      nil,
 			},
 			want:    map[string]bool{},
 			wantErr: true,
 		},
 		{
-			name: "crawls gets interrupted",
-			fields: fields{fetcher: mockFetcher{
-				webpageReader: io.NopCloser(strings.NewReader(htmlWithSingleLink)),
-				throwError:    nil,
-			}},
+			name:   "invalid max concurrency",
+			fields: fields{newMockFetcher(nil)},
+			args: args{
+				ctx:            context.Background(),
+				urlToCrawl:     *testUrl,
+				depth:          1,
+				maxConcurrency: -1,
+				errorCallback:  nil,
+				linkFound:      nil,
+			},
+			want:    map[string]bool{},
+			wantErr: true,
+		},
+		{
+			name:   "crawl with canceled context gets interrupted and should not return any link",
+			fields: fields{newMockFetcher(nil)},
 			args: args{
 				ctx:            canceledCtx,
 				urlToCrawl:     *testUrl,
-				recursionLimit: 2,
-				onNewLinkFound: nil,
+				depth:          1,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound:      nil,
 			},
 			want:    map[string]bool{},
+			wantErr: false,
+		},
+		{
+			name:   "crawl calls linkFound callback for each link found",
+			fields: fields{newMockFetcher(nil)},
+			args: args{
+				ctx:            context.Background(),
+				urlToCrawl:     *testUrl,
+				depth:          2,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound: func(link url.URL) {
+					linkFoundCh <- link
+				},
+			},
+			want: map[string]bool{
+				"https://test.com":          true,
+				"https://test.com/contact":  true,
+				"https://test.com/about-us": true,
+			},
+			wantErr: false,
+		},
+		{
+			name:   "crawl safely calls a panicking linkFound callback",
+			fields: fields{newMockFetcher(nil)},
+			args: args{
+				ctx:            context.Background(),
+				urlToCrawl:     *testUrl,
+				depth:          2,
+				maxConcurrency: 1,
+				errorCallback:  nil,
+				linkFound: func(link url.URL) {
+					linkFoundCh <- link
+					panic("")
+				},
+			},
+			want: map[string]bool{
+				"https://test.com":          true,
+				"https://test.com/contact":  true,
+				"https://test.com/about-us": true,
+			},
+			wantErr: false,
+		},
+		{
+			name:   "crawl calls error callback when an error occurs while fetching",
+			fields: fields{newMockFetcher(errors.New("error fetching"))},
+			args: args{
+				ctx:            context.Background(),
+				urlToCrawl:     *testUrl,
+				depth:          1,
+				maxConcurrency: 1,
+				errorCallback: func(link url.URL, err error) {
+					errorCallbackCh <- errorCallbackArgs{link: link, err: err}
+				},
+				linkFound: nil,
+			},
+			want: map[string]bool{
+				"https://test.com": true,
+			},
+			wantErr: false,
+		},
+		{
+			name:   "crawl safely calls a panicking error callback",
+			fields: fields{newMockFetcher(errors.New("error fetching"))},
+			args: args{
+				ctx:            context.Background(),
+				urlToCrawl:     *testUrl,
+				depth:          1,
+				maxConcurrency: 1,
+				errorCallback: func(link url.URL, err error) {
+					errorCallbackCh <- errorCallbackArgs{link: link, err: err}
+					panic("")
+				},
+				linkFound: nil,
+			},
+			want: map[string]bool{
+				"https://test.com": true,
+			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := NewConcurrent(tt.fields.fetcher)
-			got, err := c.Crawl(tt.args.ctx, tt.args.urlToCrawl, tt.args.recursionLimit, tt.args.onNewLinkFound)
+			a := NewBreadthFirstCrawler(tt.fields.fetcher)
+			got, err := a.Crawl(tt.args.ctx, tt.args.urlToCrawl, tt.args.depth, tt.args.maxConcurrency, tt.args.linkFound, tt.args.errorCallback)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Crawl() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -128,6 +254,25 @@ func TestConcurrent_Crawl(t *testing.T) {
 			for _, link := range got {
 				if _, ok := tt.want[link]; !ok {
 					t.Errorf("Crawl() link %v not found in %v", link, got)
+				}
+			}
+			if tt.args.linkFound != nil {
+				for range tt.want {
+					select {
+					case linkFromCallback := <-linkFoundCh:
+						if _, ok := tt.want[linkFromCallback.String()]; !ok {
+							t.Errorf("Crawl() linkFoundCallback executed with link %v not found in want list %v", linkFromCallback, tt.want)
+						}
+					case <-time.After(2 * time.Second):
+						t.Error("Crawl() linkFound callback not called after waiting 2 seconds")
+					}
+				}
+			}
+			if tt.args.errorCallback != nil {
+				select {
+				case <-errorCallbackCh:
+				case <-time.After(2 * time.Second):
+					t.Error("Crawl() error callback not called after waiting 2 seconds")
 				}
 			}
 		})
