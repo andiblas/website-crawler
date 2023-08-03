6a11dab85aa333dd9b82767f3fddb3e337183eeb
Andres Blas Pujadas <andres.pujadas@liveoak.bank>
2023-08-03 13:05:22 -0300

    use new bfCrawler in main

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -19,35 +19,30 @@ import (
 )
 
 const (
-	defaultRecursionLimit  = 4
+	defaultDepth           = 4
+	defaultMaxConcurrency  = 5
 	defaultTimeout         = 15000
 	defaultNumberOfRetries = 3
 )
 
 func main() {
 	urlToCrawlArg := flag.String("url", "", "URL to crawl.")
-	//recursionLimitArg := flag.Int("recursion_limit", defaultRecursionLimit, "Sets the amount of times the crawler will continue crawling on links found in a page. Must be greater than 0.")
+	depthArg := flag.Int("depth", defaultDepth, "Sets the crawling depth. The depth is delimited by each time the crawler continues crawling on new discovered pages. Must be greater than 0.")
+	maxConcurrencyArg := flag.Int("max_concurrency", defaultMaxConcurrency, "Sets the maximum concurrent requests the crawler can do. Must be greater than 0.")
 	timeoutArg := flag.Int("timeout", defaultTimeout, "Please set the timeout in milliseconds. Must be greater than 0.")
 	numberOfRetriesArg := flag.Int("retries", defaultNumberOfRetries, "Set the number of retries the crawler will try to fetch a page in case of errors. Must be 0 or greater than 0.")
 
 	flag.Parse()
 
 	timeout := validateTimeoutArg(*timeoutArg)
-	//recursionLimit := validateRecursionLimit(*recursionLimitArg)
+	depth := validateDepth(*depthArg)
+	maxConcurrency := validateMaxConcurrency(*maxConcurrencyArg)
 	parsedUrl := validateUrlToCrawl(*urlToCrawlArg)
 	numberOfRetries := validateNumberOfRetries(*numberOfRetriesArg)
 
 	httpFetcher := fetcher.NewHTTPFetcher(&http.Client{
 		Timeout: time.Duration(timeout) * time.Millisecond,
 	})
-
-	//var concurrentCrawler crawler.Crawler
-	//if numberOfRetries > 0 {
-	//	backoffRetryFetcher := fetcher.NewExpBackoffRetryFetcher(httpFetcher, numberOfRetries, time.Second*4)
-	//	concurrentCrawler = crawler.NewConcurrent(backoffRetryFetcher)
-	//} else {
-	//	concurrentCrawler = crawler.NewConcurrent(httpFetcher)
-	//}
 
 	ctx := context.Background()
 	cancelCtx, cancelFunc := context.WithCancel(ctx)
@@ -61,40 +56,23 @@ func main() {
 		cancelFunc()
 	}()
 
-	//linkCount := 0
-	//linkFoundCallback := func(link url.URL) {
-	//	linkCount++
-	//	fmt.Printf("[LINK %04d]\t%s\n", linkCount, link.String())
-	//}
-
-	// you can use the full crawling result set once finished
-	//_, err := concurrentCrawler.Crawl(cancelCtx, parsedUrl, recursionLimit, linkFoundCallback)
-	//if err != nil {
-	//	fmt.Printf("%v\n", err)
-	//}
-
-	//fmt.Printf("Total links found: %d\n", linkCount)
-
-	var anotherCrawler crawler.Crawler
+	var bfCrawler crawler.Crawler
 	if numberOfRetries > 0 {
 		backoffRetryFetcher := fetcher.NewExpBackoffRetryFetcher(httpFetcher, numberOfRetries, time.Second*4)
-		anotherCrawler = crawler.NewBreadthFirstCrawler(backoffRetryFetcher)
+		bfCrawler = crawler.NewBreadthFirstCrawler(backoffRetryFetcher)
 	} else {
-		anotherCrawler = crawler.NewBreadthFirstCrawler(httpFetcher)
+		bfCrawler = crawler.NewBreadthFirstCrawler(httpFetcher)
 	}
 
-	anotherLinkFoundCallback := func(link url.URL) {
+	linkFoundCb := func(link url.URL) {
 		fmt.Printf("[LINK] Crawling: %s\n", link.String())
 	}
-	anotherLinksFound, err := anotherCrawler.Crawl(cancelCtx, parsedUrl, 4, 10, anotherLinkFoundCallback)
+	links, err := bfCrawler.Crawl(cancelCtx, parsedUrl, depth, maxConcurrency, linkFoundCb)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for i, link := range anotherLinksFound {
-		fmt.Printf("[ANOTHER LINK %04d]\t%s\n", i, link)
-	}
-	fmt.Printf("Another Total links found: %d\n", len(anotherLinksFound))
+	fmt.Printf("Total links found: %d\n", len(links))
 }
 
 func validateUrlToCrawl(urlToCrawlArg string) url.URL {
@@ -110,11 +88,18 @@ func validateUrlToCrawl(urlToCrawlArg string) url.URL {
 	return *parsedUrl
 }
 
-func validateRecursionLimit(recursionLimitArg int) int {
-	if recursionLimitArg <= 0 {
-		log.Fatalln("argument error: invalid recursion limit. must be greater than 0. example: --recursion_limit=2")
+func validateDepth(depthArg int) int {
+	if depthArg <= 0 {
+		log.Fatalln("argument error: invalid depth. must be greater than 0. example: --depth=2")
 	}
-	return recursionLimitArg
+	return depthArg
+}
+
+func validateMaxConcurrency(maxConcurrencyArg int) int {
+	if maxConcurrencyArg <= 0 {
+		log.Fatalln("argument error: invalid max_concurrency. must be greater than 0. example: --max_concurrency=2")
+	}
+	return maxConcurrencyArg
 }
 
 func validateTimeoutArg(timeoutArg int) int {
@@ -126,7 +111,7 @@ func validateTimeoutArg(timeoutArg int) int {
 
 func validateNumberOfRetries(numberOfRetries int) int {
 	if numberOfRetries < 0 {
-		log.Fatalln("argument error: invalid retries argument. example: --retries=2")
+		log.Fatalln("argument error: invalid retries. example: --retries=2")
 	}
 	return numberOfRetries
 }
