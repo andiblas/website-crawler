43a9540fdd44b9309b4ddf43296c0272361b0131
Andrés Pujadas <andres.pujadas@liveoak.bank>
2023-08-18 17:11:51 -0300

    add options test

---
+++ pkg/crawler/options_test.go
@@ -0,0 +1,34 @@
+package crawler
+
+import (
+	"io"
+	"net/url"
+	"testing"
+)
+
+type MockFetcher struct{}
+
+func (f *MockFetcher) FetchWebpageContent(url url.URL) (io.ReadCloser, error) {
+	return nil, nil
+}
+
+func TestNewBreadthFirstCrawler(t *testing.T) {
+	mockFetcher := &MockFetcher{}
+
+	linkFoundMock := func(link url.URL) {}
+	onErrorMock := func(link url.URL, err error) {}
+
+	crawler := NewBreadthFirstCrawler(mockFetcher, WithLinkFoundCallback(linkFoundMock), WithOnErrorCallback(onErrorMock))
+
+	if crawler.fetcher != mockFetcher {
+		t.Errorf("Expected fetcher to be set to mockFetcher")
+	}
+
+	if crawler.linkFound == nil {
+		t.Errorf("Expected linkFound callback to be set")
+	}
+
+	if crawler.onError == nil {
+		t.Errorf("Expected onError callback to be set")
+	}
+}
